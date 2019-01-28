fe1e5eef47fc6ed57875dc8ba92c0fff30f73949
Varun <shrva02@ca.com>
2019-01-28 15:39:45 +0530

    NotSerializableException

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -14,69 +14,36 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	   List<Integer> inputData = new ArrayList<>();
-	   inputData.add(13);
-	   inputData.add(28);
-	   inputData.add(36);
-	   inputData.add(49);
-	   
-	   Logger.getLogger("org.apache").setLevel(Level.WARN);
-	   
-	   
-	   /*
-	    * local[*] => local means we are using Spark in local not in cluster
-	    *   [*] indicates that we don't know and use as many cores available for thread   
-	    * 
-	    * 
-	    */
-	   SparkConf conf = new SparkConf().setAppName("Jai Shri Krishna").setMaster("local[*]");
-	   
-	  /* if you just insert it means u are using only single thread 
-	   * SparkConf conf = new SparkConf().setAppName("Jai Shri Krishna").setMaster("local");*/
-	   
-	   
-	   /*
-	    * Represent a connection to Spark Cluster. This object allows to communicate the Spark
-	    */
-	   JavaSparkContext sc = new JavaSparkContext(conf);
-	   
-	   /*
-	    * Now Load some kind some data in Spark 
-	    * 
-	    * parrallelize-> Load a java collection
-	    */
-		
-	    /*
-	     * JavaRDD impleneted in Scala itself and it is bridge between Scala and 
-	     */
-	    JavaRDD<Integer> myRdd =   sc.parallelize(inputData);
-	   // myRdd.foreach(System.out::println);
-	    
-	    /*
-	     * Mapper
-	     */
-	    JavaRDD<Double> sqrtRdd = myRdd.map(value->Math.sqrt(value)); 
-	    
-	    /*
-	     * Reducer
-	     */
-	    Double result = sqrtRdd.reduce((v1,v2)->v1+v2); 
-	    
-	    /*
-	     * Printing the RDD contents
-	     * Not Recommended
-	     */
-	    sqrtRdd.foreach(value->System.out.println(value));
-	    
-	    /*
-	     * Counting the RDD
-	     */
-	    Long numberOfElement =sqrtRdd.count();
+		List<Integer> inputData = new ArrayList<>();
+		inputData.add(13);
+		inputData.add(28);
+		inputData.add(36);
+		inputData.add(49);
 
-	    System.out.println(numberOfElement );
-	    
-	    sc.close();
-	   
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
+		SparkConf conf = new SparkConf().setAppName("Jai Shri Krishna").setMaster("local[*]");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaRDD<Integer> myRdd = sc.parallelize(inputData);
+
+		/*
+		 * While uncommenting this you "may or may not " get NotSerializableException
+		 * myRdd.foreach(System.out::println);
+		 * 
+		 * But definately it will throws in distributed environmnt Reason : Since
+		 * internally there is chances that ur system contains multiple cpu and in that
+		 * case whatever function we are passing to RDD it has to be distributed thus
+		 * Spark will expect that to be; implememted Seriaizable
+		 * 
+		 * thus to avoid this error we will take help of collect() which collect all the
+		 * RDD data in one collection
+		 */
+
+		myRdd.collect().forEach(System.out::println);
+
+		JavaRDD<Double> sqrtRdd = myRdd.map(value -> Math.sqrt(value));
+
+		sc.close();
+
 	}
 
 }
