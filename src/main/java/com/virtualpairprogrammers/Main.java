d23a917cbdc58ff639d7c7bf88eae849ae9df22e
Varun <shrva02@ca.com>
2019-01-28 15:22:27 +0530

    Done

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -23,60 +23,17 @@ public class Main {
 	   Logger.getLogger("org.apache").setLevel(Level.WARN);
 	   
 	   
-	   /*
-	    * local[*] => local means we are using Spark in local not in cluster
-	    *   [*] indicates that we don't know and use as many cores available for thread   
-	    * 
-	    * 
-	    */
 	   SparkConf conf = new SparkConf().setAppName("Jai Shri Krishna").setMaster("local[*]");
-	   
-	  /* if you just insert it means u are using only single thread 
-	   * SparkConf conf = new SparkConf().setAppName("Jai Shri Krishna").setMaster("local");*/
-	   
-	   
-	   /*
-	    * Represent a connection to Spark Cluster. This object allows to communicate the Spark
-	    */
-	   JavaSparkContext sc = new JavaSparkContext(conf);
-	   
-	   /*
-	    * Now Load some kind some data in Spark 
-	    * 
-	    * parrallelize-> Load a java collection
-	    */
-		
-	    /*
-	     * JavaRDD impleneted in Scala itself and it is bridge between Scala and 
-	     */
-	    JavaRDD<Integer> myRdd =   sc.parallelize(inputData);
-	   // myRdd.foreach(System.out::println);
-	    
-	    /*
-	     * Mapper
-	     */
-	    JavaRDD<Double> sqrtRdd = myRdd.map(value->Math.sqrt(value)); 
-	    
-	    /*
-	     * Reducer
-	     */
-	    Double result = sqrtRdd.reduce((v1,v2)->v1+v2); 
-	    
-	    /*
-	     * Printing the RDD contents
-	     * Not Recommended
-	     */
-	    sqrtRdd.foreach(value->System.out.println(value));
-	    
-	    /*
-	     * Counting the RDD
-	     */
-	    Long numberOfElement =sqrtRdd.count();
+       JavaSparkContext sc = new JavaSparkContext(conf);
+	   JavaRDD<Integer> myRdd =   sc.parallelize(inputData);
 
-	    System.out.println(numberOfElement );
+	    JavaRDD<Integer>  mapRDD = myRdd.map(value->1);
+	    
+	    Integer count =mapRDD.reduce((val1,val2) -> val1+val2);
+	    
+	    System.out.println(count);
 	    
 	    sc.close();
-	   
 	}
 
 }
