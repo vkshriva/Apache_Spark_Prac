3a2c75551279615df3257ca46a37e9becc319609
Varun <shrva02@ca.com>
2019-01-31 20:36:40 +0530

    PairRDD and GroupBy

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -6,77 +6,65 @@ import java.util.List;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	   List<Integer> inputData = new ArrayList<>();
-	   inputData.add(13);
-	   inputData.add(28);
-	   inputData.add(36);
-	   inputData.add(49);
-	   
-	   Logger.getLogger("org.apache").setLevel(Level.WARN);
-	   
-	   
-	   /*
-	    * local[*] => local means we are using Spark in local not in cluster
-	    *   [*] indicates that we don't know and use as many cores available for thread   
-	    * 
-	    * 
-	    */
-	   SparkConf conf = new SparkConf().setAppName("Jai Shri Krishna").setMaster("local[*]");
-	   
-	  /* if you just insert it means u are using only single thread 
-	   * SparkConf conf = new SparkConf().setAppName("Jai Shri Krishna").setMaster("local");*/
-	   
-	   
-	   /*
-	    * Represent a connection to Spark Cluster. This object allows to communicate the Spark
-	    */
-	   JavaSparkContext sc = new JavaSparkContext(conf);
-	   
-	   /*
-	    * Now Load some kind some data in Spark 
-	    * 
-	    * parrallelize-> Load a java collection
-	    */
-		
-	    /*
-	     * JavaRDD impleneted in Scala itself and it is bridge between Scala and 
-	     */
-	    JavaRDD<Integer> myRdd =   sc.parallelize(inputData);
-	   // myRdd.foreach(System.out::println);
-	    
-	    /*
-	     * Mapper
-	     */
-	    JavaRDD<Double> sqrtRdd = myRdd.map(value->Math.sqrt(value)); 
-	    
-	    /*
-	     * Reducer
-	     */
-	    Double result = sqrtRdd.reduce((v1,v2)->v1+v2); 
-	    
-	    /*
-	     * Printing the RDD contents
-	     * Not Recommended
-	     */
-	    sqrtRdd.foreach(value->System.out.println(value));
-	    
-	    /*
-	     * Counting the RDD
-	     */
-	    Long numberOfElement =sqrtRdd.count();
+		List<String> inputData = new ArrayList<>();
+		inputData.add("WARN: Tuesday 4 September 0405");
+		inputData.add("ERROR: Tuesday 4 September 0408");
+		inputData.add("FATAL: Wednesday 5 September 1632");
+		inputData.add("ERROR: Friday 7 September 1854");
+		inputData.add("WARN: Saturday 8 September 1942");
 
-	    System.out.println(numberOfElement );
-	    
-	    sc.close();
-	   
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+		SparkConf conf = new SparkConf().setAppName("PairRDD").setMaster("local[*]");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+
+		JavaRDD<String> orignlRdd = sc.parallelize(inputData);
+
+		/*
+		 * Java PairRDD store data as key value pair is not similar to Map it allows
+		 * duplicate key
+		 */
+		JavaPairRDD<String, String> pairRdd = orignlRdd.mapToPair(rawData -> {
+			String content[] = rawData.split(":");
+			return new Tuple2<>(content[0], content[1]);
+		});
+
+		JavaPairRDD<String, Long> modifyPairRdd = pairRdd.mapToPair(rawData -> {
+			return new Tuple2<>(rawData._1, 1L);
+		});
+
+		JavaPairRDD<String, Long> resultPairRdd = modifyPairRdd.reduceByKey((val1, val2) -> val1 + val2);
+
+		resultPairRdd.collect().forEach(rawData -> {
+			System.out.println(rawData._1 + ": has " + rawData._2 + " lines");
+		});
+
+		System.out.println("******In One go****************************");
+
+		sc.parallelize(inputData).mapToPair(rawdata -> new Tuple2<String, Long>(rawdata.split(":")[0], 1L))
+				.reduceByKey((val1, val2) -> val1 + val2).collect().forEach(rawData -> {
+					System.out.println(rawData._1 + ": has " + rawData._2 + " lines");
+				});
+
+		/*
+		 * GroupBy can lead to severe perfommance problem .Avoid unless you are sure
+		 * there is no better alternative
+		 */
+      System.out.println("**************Using Group By************");
+		
+		sc.close();
+
 	}
 
 }
