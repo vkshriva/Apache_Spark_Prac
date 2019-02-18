a7f51313f4ee64aeaa1cefac5be1f17f37fa7bfb
Varun <shrva02@ca.com>
2019-02-18 22:00:13 +0530

    Done

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -37,29 +37,37 @@ public class Main {
 		SparkSession spark = SparkSession.builder().appName("Storingjavaobject").master("local[*]")
 				.config("spark.sql.warehouse.dir", "file///C:/Users/shrva02/Documents/ApacheSpark Prac/tmp")
 				.getOrCreate();
-		/*
-		 * Here u are using java object as SparkSql Object .for this List will be create
-		 * of row Type which has been created using RowFactory
-		 */
+
 		List<Row> inMemory = new ArrayList<Row>();
 		inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
 		inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
 		inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
 		inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
 		inMemory.add(RowFactory.create("FATAL", "2015-4-21 19:23:20"));
-		
-		/*
-		 * Defining each column name ,datatype and metadata 
-		 */
+
 		StructField[] fields = new StructField[] {
-				new StructField("level",DataTypes.StringType,false, Metadata.empty()),
-				new StructField("date",DataTypes.StringType,false, Metadata.empty())
-		};
-		
-		StructType schema = new StructType(fields );
-		Dataset<Row>testDataSet =spark.createDataFrame(inMemory, schema );
-		
-		testDataSet.show();
+				new StructField("level", DataTypes.StringType, false, Metadata.empty()),
+				new StructField("date", DataTypes.StringType, false, Metadata.empty()) };
+
+		StructType schema = new StructType(fields);
+		Dataset<Row> testDataSet = spark.createDataFrame(inMemory, schema);
+
+		testDataSet.createOrReplaceTempView("logging");
+
+		/*
+		 * Grouping and aggreegation here is similar we do in sql Aggregation is nothing
+		 * but group by k baad jo dataset mein operation karte hai use aggregation kehte
+		 * hai.Or naam aisse auhaa bana diya koi hadd nahi
+		 */
+		Dataset<Row> countdataset = spark.sql("select level,count(date) from logging group by level order by level");
+
+		countdataset.show();
+
+		Dataset<Row> collectListdataset = spark
+				.sql("select level,collect_list(date) from logging group by level order by level");
+
+		collectListdataset.show();
+		spark.close();
 
 	}
 
