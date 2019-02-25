b3af6659674f0390b7c0f175787fee81ccabd7d7
Varun <shrva02@ca.com>
2019-02-25 14:35:48 +0530

    Done

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -12,8 +12,10 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.RelationalGroupedDataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
@@ -34,64 +36,55 @@ public class Main {
 		System.setProperty("hadoop.home.dir", "C:\\Users\\shrva02\\Documents\\software\\winutils-extra\\hadoop");
 		Logger.getLogger("org.apache").setLevel(Level.WARN);
 
-		SparkSession spark = SparkSession.builder().appName("Storingjavaobject").master("local[*]")
+		SparkSession spark = SparkSession.builder().appName("PivotObject").master("local[*]")
 				.config("spark.sql.warehouse.dir", "file///C:/Users/shrva02/Documents/ApacheSpark Prac/tmp")
 				.getOrCreate();
-
-		List<Row> inMemory = new ArrayList<Row>();
-		inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
-		inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
-		inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
-		inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
-		inMemory.add(RowFactory.create("FATAL", "2015-4-21 19:23:20"));
-
-		StructField[] fields = new StructField[] {
-				new StructField("level", DataTypes.StringType, false, Metadata.empty()),
-				new StructField("date", DataTypes.StringType, false, Metadata.empty()) };
-
-		StructType schema = new StructType(fields);
-		Dataset<Row> testDataSet = spark.createDataFrame(inMemory, schema);
-
-		testDataSet.createOrReplaceTempView("logging");
-
-		/*
-		 * Grouping and aggreegation here is similar we do in sql Aggregation is nothing
-		 * but group by k baad jo dataset mein operation karte hai use aggregation kehte
-		 * hai.Or naam aisse auhaa bana diya koi hadd nahi
-		 */
-		Dataset<Row> countdataset = spark.sql("select level,count(date) from logging group by level order by level");
-		countdataset.show();
-
-		Dataset<Row> collectListdataset = spark
-				.sql("select level,collect_list(date) from logging group by level order by level");
-		collectListdataset.show();
-
-		/*
-		 * Changing in dateFormat
-		 */
-		Dataset<Row> dateFormatSet = spark.sql("select level,date_format(date,'MMMM')as month from logging");
-
-		dateFormatSet.show();
-
-		/*
-		 * Part 2 Based on each Month u have to give a count
-		 */
-
-		// Dataset<Row> testDataSet2 = spark.createDataFrame(inMemory, schema);
 
 		Dataset<Row> testDataSet2 = spark.read().option("header", true).csv("src/main/resources/biglog.txt");
 
 		testDataSet2.createOrReplaceTempView("LogData");
 
-		Dataset<Row> countNumberOfLevelInEachMnth = spark.sql(
-				"select level,date_format(datetime,'MMMM')as month,count(1)as Total from LogData group by level ,month");
+		Dataset<Row> dataset = spark.sql("select level,date_format(datetime,'MMMM')as month  from LogData");
+
+		// dataset.show();
 
 		/*
-		 * If you want to see more than 20 rows
+		 * PivotTable : Pivot table is helpful when you wanted to do grouping with 2
+		 * columns .Here once is level second is Month
+		 * 
 		 */
 
-		countNumberOfLevelInEachMnth.show(false); // this won't work becoz it work in column level .Not row level
-		countNumberOfLevelInEachMnth.show(100);
+		Dataset<Row> pivotDataset = dataset.groupBy("level").pivot("month").count();
+
+		pivotDataset.show(100);
+		/*
+		 * Now here Columns are not arranged in month order . In that case u can take
+		 * help of List<Object> columns
+		 */
+
+		Object month[] = new Object[] { "January", "February", "March", "April", "May", "June", "July", "August",
+				"September", "October", "November", "December" };
+		List<Object> columns = Arrays.asList(month);
+
+		Dataset<Row> pivotDatasetWithColumn = dataset.groupBy("level").pivot("month", columns).count();
+
+		pivotDatasetWithColumn.show(100);
+
+		/*
+		 * In case any Month data is not available it will come as null
+		 */
+
+		Object month2[] = new Object[] { "January", "February", "March", "April", "May", "June", "July", "August",
+				"September", "October", "November", "December", "DummyMonth" };
+		List<Object> columns2 = Arrays.asList(month2);
+
+		Dataset<Row> pivotDatasetWithColumn2 = dataset.groupBy("level").pivot("month", columns2).count();
+
+		pivotDatasetWithColumn2.show(100);
+
+		Dataset<Row> pivotDatasetWithColumn3 = dataset.groupBy("level").pivot("month", columns2).count().na().fill(0);
+
+		pivotDatasetWithColumn3.show(100);
 
 		spark.close();
 
